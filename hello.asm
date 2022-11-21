13f4db8331c2538db596508161d76e1a47bea699
Chris Sampson <csampson@appliedsystems.com>
2022-11-21 18:34:54 +0000

    put it all back together

--- hello.asm
+++ hello.asm
@@ -2,31 +2,85 @@ BITS 64
 DEFAULT REL
 
 GLOBAL main
-
 EXTERN WriteFile
 EXTERN GetStdHandle
 
-STD_OUTPUT_HANDLE   EQU -11
-
 SECTION .data
-
- l1  db "Hello world!", 13, 10, 0
- l2  dd $-l1
-
+ ds0  db "Hello world!", 10, 0
+ ds14  dq 14
+      dq ds0  
 SECTION .text
 
-main:
- sub rsp, 30h
+STD_OUTPUT_HANDLE   EQU -11
 
+main:
+ ; main :: ()
+ 
+ ; fn prologue
+ push rbp
+ mov rbp, rsp
+ 
+ ;print("hello world!\r\0");
+
+ sub rsp, 32
+ ; point to tmp string call arg
+ lea rcx, [ds14]
+
+ call print
+ add rsp, 32
+ 
+ ; fn epilogue
+ mov rsp, rbp
+ pop rbp
+ ret
+
+ print: 
+ ; print :: (to_print: string)
+
+ ; fn prologue
+ ; store old rbp and set rbp to new base
+ push rbp
+ mov rbp, rsp
+ ; store arg 1 in shadow
+ mov [rbp + 16], rcx
+ ; 36 bytes storage resvered for 5 local var: *void (8) * 4 + 1 u32 (4)
+ sub rsp, 36
+  
+ ; handle := GetStdHandle(STD_OUTPUT_HANDLE);
+ ; set shadow space, set 1 param (ECX) and call GetStdHandle
+ sub rsp, 32
  mov ecx, STD_OUTPUT_HANDLE
  call GetStdHandle
+ ; store var handle := ret val from call
+ mov [rbp - 8], eax
+ ; release shadow space for GetStdHandle proc call
+ add rsp, 32
  
- mov ecx, eax
- lea rdx, [l1]
- mov r8d, DWORD [l2]
- xor r9, r9
- mov QWORD [rsp+20h], r9
+ ; to_write := cast(*void) to_print.data;
+ mov rax, [rbp + 16]
+ mov rcx, [rax + 8]
+ mov [rbp - 16], rcx
+; length := cast(u32) to_print.len
+ mov rax, [rbp + 16]
+ mov rcx, [rax]
+ mov [rbp - 20], ecx
+ ; bytes_written: *void = null;
+ mov QWORD [rbp - 28], 0
+ ; overlapped: *void = null;
+ mov QWORD [rbp - 36], 0
+ 
+ ; set shadow space, set 5 params (one on stack) and call WriteFile
+ sub rsp, 40
+ mov ecx, [rbp - 8]
+ mov rdx, [rbp - 16]
+ mov r8d, [rbp - 20]
+ mov r9, [rbp - 28]
+ mov r10, [rbp - 36]
+ mov [rsp + 32], r10 
  call WriteFile
+ add rsp, 40
 
- add rsp, 30h
- ret  
+ ; fn epilogue
+ mov rsp, rbp
+ pop rbp
+ ret
